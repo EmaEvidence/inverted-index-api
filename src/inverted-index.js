ad25556a90c3b957ef7e089c4130c01ebecad75c
EmaEvidence <emmanuelalabi563@gmail.com>
2017-05-03 02:17:27 +0100

    Adjust inverted Index Validation

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -22,32 +22,47 @@ class InvertedIndex {
    *
    * @return {type}  description
    */
-  checkIfArrayIsValid() {
-    let result = '';
-    if (typeof this.fileContent === 'object') {
-      if (this.fileContent.length === undefined && this.fileContent[0] === undefined) {
-        result = false;
-        throw new Error('An Object but not a JSON Array');
-      } else {
-        if (this.fileContent.length === 0) {
-          result = false;
-          throw new Error('Book is Empty');
-        } else {
-          this.fileContent.forEach((file) => {
-            const fileType = typeof file.text;
-            if (fileType !== 'string') {
-              result = 'Malformed Array';
-              throw new Error('file is malformed');
-            } else {
-              result = true;
-            }
-          });
-        }
-      }
-    } else {
-      result = false;
-      throw new Error('Not an Object');
+   checkIfArrayIsValid(data) {
+     let result = '';
+       if (typeof data === 'object'){
+         if (data.length === undefined && data[0] === undefined){
+           result = false;
+           throw new Error ('An Object but not a JSON Array')
+         }
+         else{
+           if (data.length === 0) {
+             result = false;
+             throw new Error('Book is Empty');
+           } else {
+             data.forEach(function(file){
+               let fileType = typeof file.text;
+               if (fileType != 'string'){
+               result = 'Malformed Array';
+               throw new Error('file is malformed');
+           }
+           else {
+               result = true;
+           }
+         });
+           }
+         }
+       }else {
+         result = false;
+         throw new Error ('Not an Object');
+       }
+       return result;
+     }
+
+  createIndex(){
+    const file = this.fileContent;
+    let token = '';
+     if (this.checkIfArrayIsValid(file) === true){
+      console.log(file);
+    file.forEach(function(file){
+        token += (file.text)+' ';
+       });
+       token = token.replace(/[^a-zA-Z]/gi, ' ');
     }
-    return result;
+    return token;
   }
   }

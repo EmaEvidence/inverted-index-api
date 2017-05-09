01b41716156247052dc0658168ba544363edc552
EmaEvidence <emmanuelalabi563@gmail.com>
2017-05-09 10:07:48 +0100

    Create search and create index endpoints

--- src/route.js
+++ src/route.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import multer from 'multer';
-import InvertedIndex from '../dist/inverted-index';
+import InvertedIndex from '../src/inverted-index';
+
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 const invertedIndex = new InvertedIndex();
 
-app.post('/api/v0/create', (req, res) => {
-  invertedIndex.createIndex();
-  res.json('users');
+app.post('/api/v0/createIndex', (req, res) => {
+  // const a = req.single;
+  // invertedIndex.createIndex();
+  res.send('a');
 });
 
-app.post('/api/v0/search', (req, res) => {
-  invertedIndex.searchIndex();
+app.post('/api/v0/searchindex', (req, res) => {
+  /* invertedIndex.searchIndex();
   const searchResult = invertedIndex.searchIndex();
-  res.json(searchResult);
+  res.json(searchResult); */
+  res.send('me');
 });
 
 app.listen(3000);
